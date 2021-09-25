ba82ae167b03c47244569be101fa26a1aa891252
Chris Smith <chrisronsmith@gmail.com>
2021-09-24 20:11:14 -0400

    Finish timer

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from "preact/hooks";
+import { useEffect, useReducer, useState } from "preact/hooks";
 
 export function App() {
   return (
@@ -6,6 +6,7 @@ export function App() {
       <Counter />
       <TemperatureConverter />
       <FlightBooker />
+      <Timer />
     </div>
   );
 }
@@ -77,47 +78,165 @@ function FlightBooker() {
   return (
     <div class="task stack">
       <h3>Flight Booker</h3>
-      <div class="stack">
-        <select
-          name="flight"
-          id="flight"
-          onInput={(e) => setIsReturn(e.currentTarget.value === "return")}
-        >
-          <option selected={!isReturn} value="one-way">one-way flight</option>
-          <option selected={isReturn} value="return">return flight</option>
-        </select>
-        <input
-          value={format(departureDate)}
-          type="date"
-          name="start"
-          id="start"
-          onInput={(e) => setDepartureDate(new Date(e.currentTarget.value))}
-        />
-        <input
-          value={isReturn ? format(returnDate) : undefined}
-          type="date"
-          name="return"
-          id="return"
-          disabled={!isReturn}
-          onInput={(e) => setReturnDate(new Date(e.currentTarget.value))}
-        />
-        <button
-          disabled={isReturn && returnDate < departureDate}
-          onClick={() => {
-            const message = `You have booked a ${
-              isReturn
-                ? "flight"
-                : "one-way flight"
-            } ` +
-              `departing on ${format(departureDate)}` +
-              `${isReturn ? ` and returning on ${format(returnDate)}` : ""}.`;
+      <select
+        name="flight"
+        id="flight"
+        onInput={(e) => setIsReturn(e.currentTarget.value === "return")}
+      >
+        <option selected={!isReturn} value="one-way">one-way flight</option>
+        <option selected={isReturn} value="return">return flight</option>
+      </select>
+      <input
+        value={format(departureDate)}
+        type="date"
+        name="start"
+        id="start"
+        onInput={(e) => setDepartureDate(new Date(e.currentTarget.value))}
+      />
+      <input
+        value={isReturn ? format(returnDate) : undefined}
+        type="date"
+        name="return"
+        id="return"
+        disabled={!isReturn}
+        onInput={(e) => setReturnDate(new Date(e.currentTarget.value))}
+      />
+      <button
+        disabled={isReturn && returnDate < departureDate}
+        onClick={() => {
+          const message = `You have booked a ${
+            isReturn
+              ? "flight"
+              : "one-way flight"
+          } ` +
+            `departing on ${format(departureDate)}` +
+            `${isReturn ? ` and returning on ${format(returnDate)}` : ""}.`;
 
-            alert(message);
-          }}
-        >
-          Book
-        </button>
+          alert(message);
+        }}
+      >
+        Book
+      </button>
+    </div>
+  );
+}
+
+const differenceInSeconds = (start: Date, end: Date) =>
+  (end.getTime() - start.getTime()) / 1000;
+
+function Timer() {
+  type timerState = {
+    active: boolean;
+    start: Date;
+    duration: number;
+    elapsed: number;
+  };
+
+  type timerEvent =
+    | { type: "TICK" }
+    | { type: "RESET" }
+    | { type: "UPDATE_DURATION"; duration: number };
+
+  const MAX_DURATION = 100;
+
+  const reducer = (state: timerState, event: timerEvent) => {
+    switch (state.active) {
+      case true: {
+        switch (event.type) {
+          case "RESET": {
+            return {
+              ...state,
+              active: true,
+              start: new Date(),
+              elapsed: 0,
+            };
+          }
+          case "TICK": {
+            const elapsed = differenceInSeconds(state.start, new Date());
+            return {
+              ...state,
+              active: elapsed < state.duration,
+              elapsed,
+            };
+          }
+          case "UPDATE_DURATION": {
+            const elapsed = differenceInSeconds(state.start, new Date());
+            const duration = Math.min(MAX_DURATION, event.duration);
+            return {
+              ...state,
+              active: elapsed < event.duration,
+              duration,
+              elapsed,
+            };
+          }
+        }
+      }
+      case false: {
+        switch (event.type) {
+          case "RESET": {
+            return { ...state, active: true, start: new Date(), elapsed: 0 };
+          }
+          case "UPDATE_DURATION": {
+            const duration = Math.min(MAX_DURATION, event.duration);
+            return {
+              ...state,
+              active: state.elapsed < event.duration,
+              start: new Date(new Date().getTime() - duration * 1000),
+              duration,
+            };
+          }
+        }
+      }
+    }
+    return state;
+  };
+
+  const initialState = {
+    active: true,
+    start: new Date(),
+    duration: 100,
+    elapsed: 0,
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const handle = setInterval(() => {
+      dispatch({ type: "TICK" });
+    }, 50);
+    return () => clearInterval(handle);
+  }, []);
+
+  const updateDuration = (e: Event) =>
+    dispatch({
+      type: "UPDATE_DURATION",
+      duration: parseFloat((e.target as HTMLInputElement).value),
+    });
+
+  const reset = () => dispatch({ type: "RESET" });
+
+  return (
+    <div class="task stack">
+      <h3>Timer</h3>
+      <div className="row">
+        <label for="time">Elapsed time:</label>
+        <progress id="time" max={state.duration} value={state.elapsed}>
+        </progress>
       </div>
+      <p>{state.elapsed.toFixed(2)}s</p>
+      <div className="row">
+        <label for="duration">Duration:</label>
+        <input
+          type="range"
+          id="duration"
+          name="duration"
+          min="0"
+          max={MAX_DURATION}
+          value={state.duration}
+          onInput={updateDuration}
+        />
+      </div>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
