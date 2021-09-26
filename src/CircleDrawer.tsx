25c2e965f200ae3da8e57d0658939c5fdcbf89c5
Chris Smith <chrisronsmith@gmail.com>
2021-09-26 10:08:47 -0400

    Add canvas

--- src/CircleDrawer.tsx
+++ src/CircleDrawer.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useRef } from "preact/hooks"
+
 export function CircleDrawer() {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    if (!canvas) return
+    const context = canvas.getContext("2d")
+    if (!context) return
+    context.fillStyle = "black"
+    context.fillRect(0,0,canvas.width,canvas.height)
+  }, [canvasRef.current])
+
   return (
     <div class="task stack">
-      <h3>CircleDrawer</h3>
+      <h3>Circle Drawer</h3>
+      <canvas ref={canvasRef}></canvas>
     </div>
   );
 }
