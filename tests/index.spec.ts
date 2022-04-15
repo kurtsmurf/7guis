e8687e5aa01ce835e5141169bbabbf1cc47f3c1e
Chris Smith <christopher.smith@lifeomic.com>
2022-04-15 16:22:35 -0400

    Add a playwright test

---
+++ tests/index.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test'
+
+test('counter', async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+  await page.locator('button:has-text("Count")').click();
+  await page.locator('button:has-text("Count")').click();
+  await page.locator('button:has-text("Count")').click();
+  await page.locator('button:has-text("Count")').click();
+  await page.locator('button:has-text("Count")').click();
+  await expect(page.locator('text=CounterCount >> input[type="number"]')).toHaveValue('5')
+})
